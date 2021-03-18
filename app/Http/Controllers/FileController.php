dd2604651ba5931172e1feb1d99f919057c6dd15
jomskiee <delatorrejomilen@gmail.com>
2021-03-18 13:39:28 +0800

    finale

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -50,7 +50,7 @@ class FileController extends Controller
         $extension = $request->file('file')->getClientOriginalExtension();
         $fname = $request->file('file')->getClientOriginalName();
         $name = pathinfo($fname, PATHINFO_FILENAME);
-        $imagePath = $request->file('file')->store('uploads');
+        $imagePath = $request->file('file')->store('uploads', 'public');
 
         #$file = new File;
         $file->name = $name;
@@ -108,7 +108,7 @@ class FileController extends Controller
         ]);
         $extension = $request->file('file')->getClientOriginalExtension();
         $name = $request->name;
-        $imagePath = $request->file('file')->store('uploads');
+        $imagePath = $request->file('file')->store('uploads', 'public');
         
         $file->name = $name;
         $file->type = $extension;
